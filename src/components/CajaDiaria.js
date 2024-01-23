439759dc38c55e134ccc4515510bbac838bee6bf
ajax159 <gmod159@gmail.com>
2024-01-23 12:53:27 -0500

    Add rsuite library and import rsuite.min.css

--- src/components/CajaDiaria.js
+++ src/components/CajaDiaria.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react'
 import { Panel } from 'primereact/panel';
 import { Button } from 'primereact/button';
 import { Dialog } from 'primereact/dialog';
-import { FilterMatchMode, FilterOperator } from 'primereact/api';
+import { DateRangePicker } from 'rsuite';
 import { DataTable } from 'primereact/datatable';
 import { Column } from 'primereact/column';
 import { InputText } from 'primereact/inputtext';
@@ -10,9 +10,9 @@ import { Dropdown } from 'primereact/dropdown';
 import { Calendar } from 'primereact/calendar';
 import { Tag } from 'primereact/tag';
 import { Toast } from 'primereact/toast';
-import { addLocale } from 'primereact/api';
+import { FilterMatchMode } from 'primereact/api';
 import EditIcon from '@mui/icons-material/Edit';
-
+import './styles/CajaDiaria.css';
 
 let apiroute = 'https://serviciofact.mercelab.com'
 
@@ -59,11 +59,12 @@ const CajaDiaria = () => {
     const [datos, setDatos] = useState([]);
     const [date, setDate] = useState([]);
     const [selectedDato, setSelectedDato] = useState([]);
-    const [filters, setFilters] = useState(null);
+    const [filters, setFilters] = useState({
+        global: { value: null, matchMode: FilterMatchMode.CONTAINS }
+    });
     const [selectedResp, setSelectedResp] = useState(null);
     const [selectedCaj, setSelectedCaj] = useState(null);
     const [globalFilterValue, setGlobalFilterValue] = useState('');
-    const [statuses] = useState(['CERRADO', 'ABIERTO']);
 
     const getSeverity = (status) => {
         switch (status) {
@@ -75,46 +76,25 @@ const CajaDiaria = () => {
             default:
         }
     };
-    addLocale('es', {
-        firstDayOfWeek: 1,
-        showMonthAfterYear: true,
-        dayNames: ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado'],
-        dayNamesShort: ['dom', 'lun', 'mar', 'mié', 'jue', 'vie', 'sáb'],
-        dayNamesMin: ['D', 'L', 'M', 'X', 'J', 'V', 'S'],
-        monthNames: ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'],
-        monthNamesShort: ['ene', 'feb', 'mar', 'abr', 'may', 'jun', 'jul', 'ago', 'sep', 'oct', 'nov', 'dic'],
-        today: 'Hoy',
-        clear: 'Limpiar'
-    });
+    const getFetch = async (e) => {
+        const startDate = e[0].toISOString().slice(0, 10);
+        const endDate = e[1].toISOString().slice(0, 10);
+        let url = `https://serviciofact.mercelab.com/movimientocaja/listarmovcaja/1/1?fechainicio=${startDate}&fechafin=${endDate}`;
+        const response = await fetch(url);
+        let responseJSON = await response.json();
+        setDatos(responseJSON);
+    }
+    const fetchData = async () => {
+        const startDate = new Date().toISOString().slice(0, 10);
+        const endDate = new Date().toISOString().slice(0, 10);;
+        let url = `https://serviciofact.mercelab.com/movimientocaja/listarmovcaja/1/1?fechainicio=${startDate}&fechafin=${endDate}`;
+        const response = await fetch(url);
+        let responseJSON = await response.json();
+        setDatos(responseJSON);
+    };
     useEffect(() => {
-        const getFetch = async () => {
-            const response = await fetch('https://serviciofact.mercelab.com/movimientocaja/listarmovcaja/1/1');
-            let responseJSON = await response.json();
-            responseJSON = responseJSON?.map((box) => ({
-                ...box,
-                apertura: new Date(`${box.mcaFechaapertura} UTC-5`),
-                cierre: new Date(`${box.mcaFechacierre} UTC-5`)
-            }));
-            setDatos(responseJSON);
-        }
-        getFetch()
-        initFilters();
-    }, []);
-
-    const formatDate = (value) => {
-        return value.toLocaleDateString('es-MX', {
-            day: '2-digit',
-            month: '2-digit',
-            year: 'numeric'
-        });
-    };
-    const formatDatecierre = (value) => {
-        return value.toLocaleDateString('es-MX', {
-            day: '2-digit',
-            month: '2-digit',
-            year: 'numeric'
-        });
-    };
+        fetchData();
+    }, [date]);
     const onGlobalFilterChange = (e) => {
         const value = e.target.value;
         let _filters = { ...filters };
@@ -124,45 +104,31 @@ const CajaDiaria = () => {
     };
     const renderHeader = () => {
         return (
-            <div className="flex flex-wrap gap-2 justify-content-between align-items-center">
-                <span className="p-input-icon-left">
-                    <i className="pi pi-search" />
-                    <InputText value={globalFilterValue} onChange={onGlobalFilterChange} placeholder="Buscar" />
-                </span>
-                <Button label='Aperturar Caja' onClick={() => {varItems.getItems('/usuario/listar','/caja/listar/1/1')}}></Button>
+            <div className="flex flex-wrap gap-2 justify-between align-center">
+                <div className="flex align-center">
+                    <div>
+                        <span className="p-input-icon-left">
+                            <i className="pi pi-search" />
+                            <InputText value={globalFilterValue} onChange={onGlobalFilterChange} placeholder="Buscar" />
+                        </span>
+                    </div>
+                    <div style={{ marginLeft: '10px' }}>
+                        <Button label='Aperturar Caja' onClick={() => { varItems.getItems('/usuario/listar', '/caja/listar/1/1') }}></Button>
+                    </div>
+                </div>
+                <div className="flex align-center" style={{ height: '100%' }}>
+                    <DateRangePicker size="lg" placement='leftStart' onOk={e => getFetch(e)} onClean={() => fetchData()} onShortcutClick={e => getFetch(e.value)} />
+                </div>
             </div>
         );
     };
-    const dateBodyTemplate = ({ apertura }) => {
-        return formatDate(apertura);
-    };
-    const initFilters = () => {
-        setFilters({
-            global: { value: null, matchMode: FilterMatchMode.CONTAINS },
-            apertura: { operator: FilterOperator.AND, constraints: [{ value: null, matchMode: FilterMatchMode.DATE_IS }] },
-            cierre: { operator: FilterOperator.AND, constraints: [{ value: null, matchMode: FilterMatchMode.DATE_IS }] },
-            empId: { operator: FilterOperator.AND, constraints: [{ value: null, matchMode: FilterMatchMode.STARTS_WITH }] },
-            cajDescripcion: { operator: FilterOperator.AND, constraints: [{ value: null, matchMode: FilterMatchMode.STARTS_WITH }] },
-            usuNombrecompleto: { operator: FilterOperator.AND, constraints: [{ value: null, matchMode: FilterMatchMode.STARTS_WITH }] },
-            moneda: { operator: FilterOperator.AND, constraints: [{ value: null, matchMode: FilterMatchMode.STARTS_WITH }] },
-            estado: { operator: FilterOperator.OR, constraints: [{ value: null, matchMode: FilterMatchMode.EQUALS }] }
-        });
-    };
-    const dateBodyTemplatecierre = ({ cierre }) => {
-        return formatDatecierre(cierre);
-    };
-    const dateFilterTemplate = (options) => {
-        return <Calendar value={options.value} onChange={(e) => options.filterCallback(e.value, options.index)} locale='es' dateFormat="dd/mm/yy" placeholder="dd/mm/aaaa" mask="99/99/9999" />;
-    };
+
+
+
     const statusBodyTemplate = (rowData) => {
         return <Tag value={rowData.estado} severity={getSeverity(rowData.estado)} />;
     };
-    const statusFilterTemplate = (options) => {
-        return <Dropdown value={options.value} options={statuses} onChange={(e) => options.filterCallback(e.value, options.index)} itemTemplate={statusItemTemplate} placeholder="Seleccionar" className="p-column-filter" showClear />;
-    };
-    const statusItemTemplate = (option) => {
-        return <Tag value={option} severity={getSeverity(option)} />;
-    };
+
 
     const actionBodyTemplate = () => {
         return (
@@ -194,7 +160,7 @@ const CajaDiaria = () => {
     ///////////////////////////////////////////////////////
     const toast = React.useRef(null);
     const aperturarCaja = () => {
-        const fechaApertura = date.toISOString().slice(0, 10);
+        const fechaApertura = date;
 
         fetch(`${apiroute}/movimientocaja/crear`, {
             method: 'POST',
@@ -214,6 +180,7 @@ const CajaDiaria = () => {
             })
         }).then(response => {
             if (response.ok) {
+                fetchData();
                 response.json().then(data => {
                     guardarMov(data.data.mcaId, data.data.mcaTipomovimiento, data.data.mcaTotal);
                 });
@@ -242,6 +209,7 @@ console.log('error')
         })
     }
     ///////////////////////////////////////////////////////
+
     return (
         <div>
             <Panel header={headerPanel} className='px-1 pt-2' toggleable>
@@ -307,13 +275,13 @@ console.log('error')
                         currentPageReportTemplate="Showing {first} to {last} of {totalRecords} entries"
                     >
                         <Column selectionMode="multiple" headerStyle={{ width: '3rem' }}></Column>
-                        <Column field="apertura" header="Apertura" sortable filterField="apertura" dataType="date" style={{ minWidth: '10rem' }} body={dateBodyTemplate} filter filterElement={dateFilterTemplate} />
-                        <Column field="cierre" header="Cierre" sortable filterField="cierre" dataType="date" style={{ minWidth: '10rem' }} body={dateBodyTemplatecierre} filter filterElement={dateFilterTemplate} />
-                        <Column field="empId" header="Sucursal" sortable filter filterPlaceholder="Buscar por sucursal" style={{ minWidth: '14rem' }} />
-                        <Column field="cajDescripcion" header="Caja" sortable filter filterPlaceholder="Buscar por caja" style={{ minWidth: '10rem' }} />
-                        <Column field="usuNombrecompleto" header="Encargado" sortable filter filterPlaceholder="Buscar por encargado" style={{ minWidth: '12rem' }} />
-                        <Column field="moneda" header="Moneda" sortable filter filterPlaceholder="Buscar por moneda" style={{ minWidth: '9rem' }} />
-                        <Column field="estado" header="Estado" sortable filterMenuStyle={{ width: '14rem' }} style={{ minWidth: '9rem' }} body={statusBodyTemplate} filter filterElement={statusFilterTemplate} />
+                        <Column field="mcaFechaapertura" header="Apertura" sortable dataType="date" style={{ minWidth: '10rem' }}  />
+                        <Column field="mcaFechacierre" header="Cierre" sortable filterField="cierre" dataType="date" style={{ minWidth: '10rem' }} />
+                        <Column field="empId" header="Sucursal" sortable style={{ minWidth: '14rem' }} />
+                        <Column field="cajDescripcion" header="Caja" sortable style={{ minWidth: '10rem' }} />
+                        <Column field="usuNombrecompleto" header="Encargado" sortable style={{ minWidth: '12rem' }} />
+                        <Column field="moneda" header="Moneda" sortable style={{ minWidth: '9rem' }} />
+                        <Column field="estado" header="Estado" sortable filterMenuStyle={{ width: '14rem' }} style={{ minWidth: '9rem' }} body={statusBodyTemplate} />
                         <Column headerStyle={{ minWidth: '4rem' }} body={actionBodyTemplate} />
                     </DataTable>
                 </div>
