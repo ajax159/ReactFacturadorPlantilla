9a7057391c17daf46838eca044c5515b58776369
ajax159 <gmod159@gmail.com>
2024-02-14 09:59:14 -0500

    Add buttons for sending and canceling in RendiciondeCaja component

--- src/components/RendiciondeCaja.js
+++ src/components/RendiciondeCaja.js
@@ -5,8 +5,24 @@ import 'primeflex/primeflex.css';
 import './styles/RendiciondeCaja.css';
 import Ingresos from './RendicionCaja/Ingresos.js';
 import Egresos from './RendicionCaja/Egresos.js';
+import Button from '@mui/material/Button';
 export const rendicionCaja = (
-    <div id="pr_id_4_header" class="p-dialog-title" data-pc-section="headertitle">Rendicion de Caja</div>
+    
+    <div className='flex'>
+        <div className=' mr-3'>
+        <div id="pr_id_4_header" class="p-dialog-title" data-pc-section="headertitle">Rendicion de Caja</div>
+        </div>
+        <div className='mr-3'>
+        <Button variant="contained" color="success">
+        Enviar
+      </Button>
+        </div>
+        <div className='mr-3'>
+        <Button variant="contained" color="error">
+        Cancelar
+      </Button>
+        </div>
+    </div>
 )
 const RendiciondeCaja = () => {
 
